cbfe2459df17373885ba18b475bbb4ca92ad0ff1
Colton Swapp <coltonbswapp@gmail.com>
2020-09-08 13:48:54 -0600

    changed model and model controller

--- KnowYourCar/Models/ Car.swift
+++ KnowYourCar/Models/ Car.swift
@@ -10,13 +10,21 @@ import Foundation
 
 class Car {
 	let name: String
+    let overview: String
 	let stat1: String
-	let carImage: String
+    let stat2: String
+    let stat3: String
+	let carImageDriving: String
+    let carImageStill: String
 	
-	init(name: String, stat1: String, carImage: String) {
+    init(name: String, overview: String, stat1: String, stat2: String, stat3: String, carImageDriving: String, carImageStill: String) {
 		self.name = name
+        self.overview = overview
 		self.stat1 = stat1
-		self.carImage = carImage
-		
+        self.stat2 = stat2
+        self.stat3 = stat3
+		self.carImageDriving = carImageDriving
+        self.carImageStill = carImageStill
+    
 	}
 }
